7ae9fc3a9aa0738935ebe5cced18cd4f3e150b46
robinpokorny <robin.pokorny@productboard.com>
2024-12-03 11:37:47 +0100

    :zap: day 1, part 1

---
+++ src/Day01.kt
@@ -0,0 +1,38 @@
+import kotlin.math.abs
+
+private fun parse(input: List<String>): Pair<List<Int>, List<Int>> = input
+    .map { it.split("\\s+".toRegex()).let { it[0].toInt() to it[1].toInt() } }
+    .unzip()
+
+
+private fun part1(input: Pair<List<Int>, List<Int>>): Int =
+
+     input.first.sorted().zip(input.second.sorted()).sumOf { (x, y) -> abs(x - y) }
+
+
+private fun part2(input: Pair<List<Int>, List<Int>>): Int {
+    return 0
+}
+
+fun main() {
+    val testInput = parse(rawTestInput)
+    println(testInput)
+    val input = parse(readDayInput(1))
+
+    // PART 1
+    assertEquals(part1(testInput), 11)
+    println("Part1: ${part1(input)}")
+
+    // PART 2
+    //assertEquals(part2(testInput), 0)
+    //println("Part2: ${part2(input)}")
+}
+
+private val rawTestInput = """
+    3   4
+    4   3
+    2   5
+    1   3
+    3   9
+    3   3
+""".trimIndent().lines()
