fbad772787bbf86b2c2e9f681880401b8d23fef5
robinpokorny <robin.pokorny@productboard.com>
2024-12-04 10:59:54 +0100

    :zap: day 3, part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,15 +1,42 @@
+sealed class Instruction {
+  object Do : Instruction()
+
+  object Dont : Instruction()
+
+  data class Mul(val a: Int, val b: Int) : Instruction()
+}
+
 private fun parse(input: List<String>) =
-    Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)")
+    Regex("""do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)""")
         .findAll(input.joinToString(""))
-        .map { it.groups[1]!!.value.toInt() to it.groups[2]!!.value.toInt() }
+        .map { matchResult ->
+          when (matchResult.groupValues[0]) {
+            "do()" -> Instruction.Do
+            "don't()" -> Instruction.Dont
+            else ->
+                Instruction.Mul(
+                    matchResult.groupValues[1].toInt(),
+                    matchResult.groupValues[2].toInt())
+          }
+        }
         .toList()
 
-private fun part1(input: List<Pair<Int, Int>>): Int =
-    input.sumBy { (a, b) -> a * b }
+private fun part1(input: List<Instruction>): Int =
+    input.filterIsInstance<Instruction.Mul>().sumOf { it.a * it.b }
 
-private fun part2(input: List<String>): Int {
-  return 0
-}
+private fun part2(input: List<Instruction>): Int =
+    input
+        .fold(0 to true) { (acc, enabled), instruction ->
+          when (instruction) {
+            is Instruction.Do -> acc to true
+            is Instruction.Dont -> acc to false
+            is Instruction.Mul -> {
+              if (enabled) (acc + (instruction.a * instruction.b) to enabled)
+              else acc to enabled
+            }
+          }
+        }
+        .first
 
 fun main() {
   val testInput = parse(rawTestInput)
@@ -20,13 +47,13 @@ fun main() {
   println("Part1: ${part1(input)}")
 
   // PART 2
-  // assertEquals(part2(testInput), 0)
-  // println("Part2: ${part2(input)}")
+  assertEquals(part2(testInput), 48)
+  println("Part2: ${part2(input)}")
 }
 
 private val rawTestInput =
     """
-    xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
-"""
+    xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+ """
         .trimIndent()
         .lines()
