722f65fcc769143157cdf1cdb728dda2712312a1
robinpokorny <robin.pokorny@productboard.com>
2024-12-07 11:43:53 +0100

    :zap: day 7, part 2

--- src/Day07.kt
+++ src/Day07.kt
@@ -9,6 +9,7 @@ private fun parse(input: List<String>): List<Pair<Long, List<Long>>> =
 private fun solve(
     result: Long,
     equation: List<Long>,
+    operators: List<(Long, Long) -> Long>,
     acc: Long = 0,
     index: Int = 0
 ): Boolean {
@@ -17,18 +18,26 @@ private fun solve(
 
   val next = equation[index]
 
-  return solve(result, equation, acc + next, index + 1) ||
-      solve(result, equation, acc * next, index + 1)
+  return operators.any { op ->
+    solve(result, equation, operators, op(acc, next), index + 1)
+  }
 }
+
+private fun concat(a: Long, b: Long): Long = "$a$b".toLong()
 
 private fun part1(input: List<Pair<Long, List<Long>>>): Long =
     input
-        .filter { (result, equation) -> solve(result, equation) }
+        .filter { (result, equation) ->
+          solve(result, equation, listOf(Long::plus, Long::times))
+        }
         .fold(0L) { acc, pair -> acc + pair.first }
 
-private fun part2(input: List<Pair<Long, List<Long>>>): Int {
-  return 0
-}
+private fun part2(input: List<Pair<Long, List<Long>>>): Long =
+    input
+        .filter { (result, equation) ->
+          solve(result, equation, listOf(Long::plus, Long::times, ::concat))
+        }
+        .fold(0L) { acc, pair -> acc + pair.first }
 
 fun main() {
   val testInput = parse(rawTestInput)
@@ -39,7 +48,7 @@ fun main() {
   println("Part1: ${part1(input)}")
 
   // PART 2
-  assertEquals(part2(testInput), 0)
+  assertEquals(part2(testInput), 11387L)
   println("Part2: ${part2(input)}")
 }
 
