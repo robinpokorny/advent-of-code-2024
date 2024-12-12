bea542fa93552fa5fb89107b3e9e71a6882f34bb
robinpokorny <robin.pokorny@productboard.com>
2024-12-12 14:53:48 +0100

    :zap: day 8, part 1

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,49 +1,55 @@
-private typealias Antennas = Map<Char, List<Point>>
+private typealias AntennaMap = Map<Char, List<Point>>
 
-private fun parse(input: List<String>): Antennas = input
-    .flatMapIndexed { y, line ->
-        line.mapIndexedNotNull { x, c ->
-            if (c != '.') c to Point(x, y) else null
-        }
-    }
-    .groupBy({ it.first }, { it.second })
-
-
-private fun part1(input: Antennas): Int {
-    input.map { (freq, antennas) ->
-        val pairs = antennas.flatMap { a ->
-            antennas.mapNotNull { b ->
-                if (a != b) a to b else null
+private fun parse(input: List<String>): Pair<Int, AntennaMap> =
+    input.size to
+        input
+            .flatMapIndexed { y, line ->
+              line.mapIndexedNotNull { x, c ->
+                if (c != '.') c to Point(x, y) else null
+              }
             }
-        }
+            .groupBy({ it.first }, { it.second })
 
-        val distances = pairs.map { (a, b) ->
-            listOf(a )
+private fun part1(input: Pair<Int, AntennaMap>): Int {
+  val (size, antennaMap) = input
+
+  return antennaMap.values
+      .flatMap<List<Point>, Point> { antennas ->
+        val pairs =
+            antennas.flatMapIndexed { i, a ->
+              antennas.drop(i).mapNotNull { b -> if (a != b) a to b else null }
+            }
+
+        pairs.flatMap { (a, b) ->
+          val vector = a - b
+          if (a.y > b.y) listOf(a - vector, b + vector)
+          else listOf(a + vector, b - vector)
         }
-    }
-    
-    return 0
+      }
+      .toSet()
+      .filter { point -> point.x in 0 until size && point.y in 0 until size }
+      .size
 }
 
-private fun part2(input: Antennas): Int {
-    return 0
+private fun part2(input: Pair<Int, AntennaMap>): Int {
+  return 0
 }
 
 fun main() {
-    val testInput = parse(rawTestInput)
-    println(testInput)
-    val input = parse(readDayInput(8))
+  val testInput = parse(rawTestInput)
+  val input = parse(readDayInput(8))
 
-    // PART 1
-    assertEquals(part1(testInput), 14)
-    println("Part1: ${part1(input)}")
+  // PART 1
+  assertEquals(part1(testInput), 14)
+  println("Part1: ${part1(input)}")
 
-    // PART 2
-    assertEquals(part2(testInput), 0)
-    println("Part2: ${part2(input)}")
+  // PART 2
+  assertEquals(part2(testInput), 0)
+  println("Part2: ${part2(input)}")
 }
 
-private val rawTestInput = """
+private val rawTestInput =
+    """
     ............
     ........0...
     .....0......
@@ -56,4 +62,6 @@ private val rawTestInput = """
     .........A..
     ............
     ............
-""".trimIndent().lines()
+"""
+        .trimIndent()
+        .lines()
